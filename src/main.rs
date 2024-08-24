2d8c9abbb41ae9d83ee1878f317a1e5da66b7324
ad-world <67097683+ad-world@users.noreply.github.com>
2024-08-23 23:12:30 -0400

    feat: add -m flag, and support no args

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
-use std::{env, fs::File, io::{BufRead, BufReader}, process::exit};
+use std::{env, fs::{self, File}, io::{BufRead, BufReader}, process::exit};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Usage: rust-wc -[c|l|w|m] <filename>");
         return;
+    }
+
+    if !is_flag(args[1].clone()) {
+        // default option, file is first argument
+
+        let filename = &args[1];
+        let file = match File::open(filename) {
+            Ok(file) => file,
+            Err(_) => {
+                println!("Error: File {} not found", filename);
+                exit(1);
+            }
+        };
+
+        let content = fs::read_to_string(filename).unwrap();
+
+        println!("{} {} {} {}", get_bytes(&content), get_lines(&content), get_words(&content), filename);
+        exit(0);
     }
 
     let flag = get_flag(args[1].clone());
@@ -21,42 +39,39 @@ fn main() {
         }
     };
 
+    let filecontent = fs::read_to_string(filename).unwrap();
+
     if flag.unwrap() == 'c' {
-        let bytes = get_bytes(file);
+        let bytes = get_bytes(&filecontent);
         println!("{} {}", filename, bytes);
     } else if flag.unwrap() == 'l' {
-        let lines = get_lines(file);
+        let lines = get_lines(&filecontent);
         println!("{} {}", filename, lines);
     } else if flag.unwrap() == 'w' {
-        let words = get_words(file);
+        let words = get_words(&filecontent);
         println!("{} {}", filename, words);
     } else if flag.unwrap() == 'm' {
-        // count_chars(file, filename);
+        let chars = get_chars(filename);
+        println!("{} {}", filename, chars);
     }
 
 
 }
 
-fn get_bytes(f: File) -> u64 {
-    return f.metadata().unwrap().len();
+fn get_bytes(f: &String) -> usize {
+    return f.bytes().len();
 }
 
-fn get_lines(f: File) -> usize {
-    return BufReader::new(f).lines().count();
+fn get_lines(f: &String) -> usize {
+    return f.split('\n').count()
 }
 
-fn get_words(f: File) -> usize {
-    let words: BufReader<_> = BufReader::new(f);
-        
-    let mut count = 0;
+fn get_words(f: &String) -> usize {
+    return f.split_whitespace().count();
+}
 
-    for line in words.lines() {
-        let line = line.unwrap();
-        let words = line.split_whitespace();
-        count += words.count();
-    }
-
-    return count;
+fn get_chars(f: &String) -> usize {
+   return f.chars().count();
 }
 
 
@@ -72,4 +87,12 @@ fn get_flag(arg: String) -> Result<char, bool> {
             return Err(false);
         }
     }
+}
+
+fn is_flag(arg: String) -> bool {
+    if arg.chars().nth(0).unwrap() == '-' {
+        return true;
+    }
+
+    return false;
 }
